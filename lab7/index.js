6c45a498bdf6dc2ff1a11b498ac08ddf907c2599
PiotrWasak <68294222+PiotrWasak@users.noreply.github.com>
2021-12-19 12:06:50 +0100

    zad 3

--- lab7/index.js
+++ lab7/index.js
@@ -2,11 +2,28 @@ const axios = require("axios");
 const functions = {
   sum: (a, b) => a + b,
   fetchUsers: async () => {
-    const response =  await axios.get(
+    const response = await axios.get(
       "https://jsonplaceholder.typicode.com/users"
     );
     return response;
   },
+  fetchUser: async (id) => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    return response;
+  },
+  invalidRequest: async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/userss/"
+    );
+    return response;
+  },
+  postUser: async (userData) => {
+    const response = axios.post("/posts")
+  }
 };
 
 module.exports = functions;
+
+
--- lab7/index.test.js
+++ lab7/index.test.js
@@ -1,23 +1,60 @@
 const axios = require("axios");
-const functions = require('./index');
+const functions = require("./index");
 
-test("Status code should be 200", async () =>{
+test("Status code should be 200", async () => {
+  const response = await functions.fetchUsers();
+  expect(response.status).toBe(200);
+});
+
+test("Response should have data", async () => {
+  const response = await functions.fetchUsers();
+  expect(response.data).toBeTruthy();
+});
+
+test("Should be at least 10 users", async () => {
+  const response = await functions.fetchUsers();
+  expect(response.data.length).toBeGreaterThanOrEqual(10);
+});
+
+test("First user should be Leanne Graham", async () => {
+  const response = await functions.fetchUsers();
+  expect(response.data[0].name).toEqual("Leanne Graham");
+});
+
+test("First user should be Leanne Graham", async () => {
+  const response = await functions.fetchUsers();
+  expect(response.data[0].name).toEqual("Leanne Graham");
+});
+
+test("Clementine Bauch's geo should be object with lat: -68.6102, lng: 47.0653", async () => {
+  const response = await functions.fetchUser(3);
+  expect(response.data.address.geo).toEqual({
+    lat: "-68.6102",
+    lng: "-47.0653",
+  });
+});
+
+test(
+  "All users should have 6 keys: " +
+    "'id', 'name',\n" +
+    "  'username', 'email',\n" +
+    "  'address',  'phone',\n" +
+    "  'website',  'company'",
+  async () => {
     const response = await functions.fetchUsers();
-    expect(response.status).toBe(200);
-})
-
-test("Response should have data", async () =>{
-    const response = await functions.fetchUsers();
-    expect(response.data).toBeTruthy();
-})
-
-
-test("Should be at least 10 users", async () =>{
-    const response = await functions.fetchUsers();
-    expect(response.data.length).toBeGreaterThanOrEqual(10);
-})
-
-test("First user should be Leanne Graham", async () =>{
-    const response = await functions.fetchUsers();
-    expect(response.data[0].name).toEqual("Leanne Graham");
-})
+    let keys;
+    response.data.forEach((user, index) => {
+      keys = Object.keys(user);
+    });
+    expect(keys).toEqual([
+      "id",
+      "name",
+      "username",
+      "email",
+      "address",
+      "phone",
+      "website",
+      "company",
+    ]);
+  }
+);
